079863aa7e5c06d989feb2a4d7c8df6ceb82da4d
Xuanhao <shirou-emiya2002@outlook.com>
2023-10-26 12:11:15 +0800

    修正命令中文件名称大小写细节

--- onnx_video2anime_pyav.py
+++ onnx_video2anime_pyav.py
@@ -134,7 +134,7 @@ if __name__ == '__main__':
     # python onnx_video2anime_pyav.py --video examples/2.mp4 --output output --model JP_face_v1.0 --onnx pb_and_onnx_model/AnimeGANv3_JP_face_v1.0.onnx 日漫脸
     # python onnx_video2anime_pyav.py --video examples/2.mp4 --output output --model PortraitSketch_25 --onnx pb_and_onnx_model/AnimeGANv3_PortraitSketch_25.onnx 素描
     # 加密
-    # python onnx_video2anime_pyav.py --video examples/2.mp4 --output output --model H64_model0 --onnx pb_and_onnx_model/Animeganv3_H64_model0.onnx 宫崎骏
+    # python onnx_video2anime_pyav.py --video examples/2.mp4 --output output --model H64_model0 --onnx pb_and_onnx_model/AnimeGANv3_H64_model0.onnx 宫崎骏
 
     arg = parse_args()
     check_folder(arg.output)
